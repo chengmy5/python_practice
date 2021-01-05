1d89bfe3d023298d93094f95be3d4f460e044eaa
chengmy <chengmy5@163.com>
2021-01-05 18:04:33 +0800

    获取中国移动招标公告

--- bidding_information_01.py
+++ bidding_information_01.py
@@ -1,17 +1,11 @@
 from selenium import webdriver
 import time
-driver_path=r'D:\driver\chromedriver.exe'
-# driver_path=r'D:\driver\IEDriverServer.exe'
-# driver_path=r'D:\driver\msedgedriver.exe'
-driver=webdriver.Chrome(executable_path=driver_path)
-# driver=webdriver.Ie(executable_path=driver_path)
-# driver=webdriver.Edge(executable_path=driver_path)
+
+driver_path=r'D:\driver\IEDriverServer.exe'
+driver=webdriver.Ie(executable_path=driver_path)
 url="https://b2b.10086.cn/b2b/main/showBiao!preShowBiao.html?noticeType=list1"
 driver.get(url)
 time.sleep(3)
 content=driver.page_source
-# next=driver.find_elements_by_css_selector('#pageid2 > table > tbody > tr > td')
-# next.click()
-# driver.close()
+driver.quit()
 print(content)
-# print(next)
